6d872bc302bc03f4251b80cd58efb7e3550a6db4
charley91107 <143663776+charley91107@users.noreply.github.com>
2025-10-21 20:54:22 +0200

    finished gifs app

--- REACT/03-gifs-app/src/GifsApp.tsx
+++ REACT/03-gifs-app/src/GifsApp.tsx
@@ -1,42 +1,17 @@
-import { useState } from "react"
-import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action"
-import GifList from "./gifs/components/GifList"
-import { PreviousSearches } from "./gifs/components/PreviousSearches"
-import { CustomHeader } from "./shared/components/CustomHeader"
-import { SearchBar } from "./shared/components/SearchBar"
-import type { Gif } from "./gifs/interfaces/gif.interface"
-
-
+import GifList from "./gifs/components/GifList";
+import { PreviousSearches } from "./gifs/components/PreviousSearches";
+import { useGifs } from "./gifs/hooks/useGifs";
+import { CustomHeader } from "./shared/components/CustomHeader";
+import { SearchBar } from "./shared/components/SearchBar";
 
 export const GifsApp = () => {
 
-    const [gifs, setGifs] = useState<Gif[]>([]);
-    const [previousTerms, setPreviousTerms] =  useState<string[]>([]);
-
-    const handleTermClicked = ( term: string ) => {
-        console.log({term});
-    }
-
-    const handleSearch = async (query: string = '') => {
-        const term = query.trim().toLowerCase();
-        if(!query){
-            return;
-        }
-
-        if (previousTerms.includes(term)){
-            return;
-        };
-        
-        setPreviousTerms((serie) => {
-            const update = [term, ...serie];
-            return update.slice(0, 8);
-        });
-
-        const gifs = await getGifsByQuery(query);
-
-        setGifs(gifs);
-
-    }
+    const {
+        gifs,
+        previousTerms,
+        handleTermClicked,
+        handleSearch 
+    } = useGifs();
 
     return (
         <>
