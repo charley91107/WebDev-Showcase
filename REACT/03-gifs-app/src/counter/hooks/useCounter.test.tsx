77aeda5f2469e2e7ddcf08a3097e10707a610400
charley91107 <143663776+charley91107@users.noreply.github.com>
2025-11-03 21:54:26 +0100

    customHooks

---
+++ REACT/03-gifs-app/src/counter/hooks/useCounter.test.tsx
@@ -0,0 +1,60 @@
+import { act, renderHook } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
+import { useCounter } from "./useCounter";
+
+
+describe ('useCounter', ()=>{
+    test('should initialize with default value of 10', ()=> {
+        const  { result } = renderHook( () => useCounter(10) );
+
+        expect(result.current.counter).toBe(10);
+    });
+
+    test('should initialize with value 20', ()=> {
+        const initialValue = 20;
+
+        const { result } = renderHook(() => useCounter(initialValue));
+
+        expect(result.current.counter).toBe(initialValue);
+    });
+
+    test('should increment counter when handleAdd is called', () => {
+        const { result } = renderHook(() => useCounter());
+
+        act(() => {
+            result.current.handleAdd();
+        });
+        
+        expect(result.current.counter).toBe(11);
+    });
+
+    test('should decrement counter when handleSubtract is called', () => {
+        const { result } = renderHook(() => useCounter());
+
+        act(() => {
+            result.current.handleSubtract();
+        });
+        
+        expect(result.current.counter).toBe(9);
+    });
+
+    test('should restart counter when handleReset is called', () => {
+        const { result } = renderHook(() => useCounter());
+
+        act(() => {
+            result.current.handleSubtract();
+            result.current.handleSubtract();
+            result.current.handleSubtract();
+            result.current.handleSubtract();
+            result.current.handleSubtract();
+        });
+
+        expect(result.current.counter).toBe(5);
+
+        act(() => {
+            result.current.handleReset();
+        })
+        
+        expect(result.current.counter).toBe(10);
+    })
+});
